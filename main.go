1d1fdc33eee93dbdca7bb868a243be455fb4bee9
ngthecoder <ngoto0208@gmail.com>
2024-05-03 22:49:17 -0400

    loaded .env

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT environment variable not set")
+	}
+
+	fmt.Println(portString)
 }
