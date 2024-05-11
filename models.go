c981abd043f47ef3fa669e9ff32cc15bffd53bc9
ngthecoder <ngoto0208@gmail.com>
2024-05-11 22:53:02 +0900

    added a fucntion to convert database.User to User

--- models.go
+++ models.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/ngthecoder/rss_scraper/internal/database"
 )
 
 type User struct {
@@ -11,4 +12,13 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 	Name      string    `json:"name"`
+}
+
+func databaseUserToUser(dbUser database.User) User {
+	return User{
+		ID:        dbUser.ID,
+		CreatedAt: dbUser.CreatedAt,
+		UpdatedAt: dbUser.UpdatedAt,
+		Name:      dbUser.Name,
+	}
 }
