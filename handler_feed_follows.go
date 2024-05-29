cd2ec32a1cd23b3eb467e0e9e62370c45fdaea10
ngthecoder <ngoto0208@gmail.com>
2024-05-30 00:46:37 +0900

    added new func to get feed follows

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -37,3 +37,13 @@ func (apiCfg *apiConfig) handlerCreateFeedFollow(w http.ResponseWriter, r *http.
 
 	respondWithJSON(w, 201, databaseFeedFollowToFeedFollow(feedFollow))
 }
+
+func (apiCfg *apiConfig) handlerGetFeedFollows(w http.ResponseWriter, r *http.Request, user database.User) {
+	feedFollows, err := apiCfg.DB.GetFeedFollows(r.Context(), user.ID)
+	if err != nil {
+		respondWithError(w, 500, fmt.Sprintf("Could not get feed follows: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 200, databaseFeedFollowsToFeedFollows(feedFollows))
+}
