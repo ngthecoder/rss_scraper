3de400419fbcbbec11ab3be342ef14db834e2081
ngthecoder <ngoto0208@gmail.com>
2024-05-30 19:04:26 +0900

    added new handler to delete feed follow

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/go-chi/chi"
 	"github.com/google/uuid"
 	"github.com/ngthecoder/rss_scraper/internal/database"
 )
@@ -46,4 +47,22 @@ func (apiCfg *apiConfig) handlerGetFeedFollows(w http.ResponseWriter, r *http.Re
 	}
 
 	respondWithJSON(w, 200, databaseFeedFollowsToFeedFollows(feedFollows))
+}
+
+func (apiCfg *apiConfig) handlerDeleteFeedFollows(w http.ResponseWriter, r *http.Request, user database.User) {
+	feedFollowID, err := uuid.Parse(chi.URLParam(r, "feedFollowID"))
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't parse feed follow id: %v", err))
+		return
+	}
+
+	err = apiCfg.DB.DeleteFeedFollow(r.Context(), database.DeleteFeedFollowParams{
+		ID:     feedFollowID,
+		UserID: user.ID,
+	})
+	if err != nil {
+		respondWithError(w, 500, fmt.Sprintf("Could not delete feed follow: %v", err))
+		return
+	}
+	respondWithJSON(w, 200, struct{}{})
 }
